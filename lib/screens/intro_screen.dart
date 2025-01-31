8caf4504711e02a0d6c2363f720e341506f57f54
jovan <git.keseljevicjovan@proton.me>
2025-01-31 04:07:29 +0100

    Update intro page

--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -5,41 +5,100 @@ import 'register_screen.dart';
 class IntroScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('EcoConnect', style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
-            SizedBox(height: 20),
-            AnimatedOpacity(
-              opacity: 1.0,
-              duration: Duration(seconds: 1),
-              child: Column(
-                children: [
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => LoginScreen()),
-                      );
-                    },
-                    child: Text('Prijava', style: TextStyle(fontSize: 20)),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => RegisterScreen()),
-                      );
-                    },
-                    child: Text('Registracija', style: TextStyle(fontSize: 20)),
-                  ),
-                ],
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/2.png'),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ],
-        ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'EcoConnect',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w900,
+                    fontSize: width * 0.1,
+                    color: Color(0xFF1B5E20),
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+                SizedBox(height: height * 0.05),
+                Text(
+                  'Vaš digitalni kompas',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w500,
+                    fontSize: width * 0.05,
+                    color: Color(0xFF1B5E20),
+                  ),
+                ),
+                SizedBox(height: height * 0.1),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => RegisterScreen()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xCC1B5E20),
+                    minimumSize: Size(width * 0.6, height * 0.07),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  child: Text(
+                    'Registracija',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.bold,
+                      fontSize: width * 0.05,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                SizedBox(height: height * 0.05),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xCC1B5E20),
+                    minimumSize: Size(width * 0.6, height * 0.07),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  child: Text(
+                    'Prijava',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.bold,
+                      fontSize: width * 0.05,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
