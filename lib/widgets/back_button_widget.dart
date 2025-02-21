af37bac2345acca568a519cca5e3e19cd99d7ed7
TodorW <vuk.todorovic@sss-nk.edu.me>
2025-02-21 21:27:50 +0100

    positioned arrow actually

--- lib/widgets/back_button_widget.dart
+++ lib/widgets/back_button_widget.dart
@@ -3,21 +3,19 @@ import 'package:flutter/material.dart';
 class BackButtonWidget extends StatelessWidget {
   final double size;
   final Color color;
-
-  const BackButtonWidget({this.size = 38, this.color = Colors.black, Key? key}) : super(key: key);
+  
+  BackButtonWidget({this.size = 38, this.color = Colors.white});
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.topLeft, 
-      child: Padding(
-        padding: const EdgeInsets.all(16.0), 
-        child: IconButton(
-          icon: Icon(Icons.arrow_back, size: size, color: color),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
+    return Positioned(
+      bottom: 16,
+      left: MediaQuery.of(context).size.width * 0.5 - (size / 2),
+      child: IconButton(
+        icon: Icon(Icons.arrow_back, size: size, color: color),
+        onPressed: () {
+          Navigator.pop(context);
+        },
       ),
     );
   }
